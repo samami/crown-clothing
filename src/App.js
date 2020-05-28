88375e584ef5fe2f1d6218c9371c9b3ab31b84c7
Sam Valenti <sam@arts-people.com>
2020-05-28 07:03:39 -1000

    Add Collection item

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
+import Shoppage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Homepage} />
-      <Route path="/hats" component={HatsPage} />
+      <Route path="/shop" component={Shoppage} />
     </Switch>
   );
 }
