de4a45583aac7598703e195a48653694b5cd6c91
Sam Valenti <sam@arts-people.com>
2020-06-01 09:19:42 -1000

    Add input fields and custom button components with styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 import Shoppage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import Entrypage from "./pages/entrypage/entrypage.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={Shoppage} />
+        <Route path="/entrypage" component={Entrypage} />
       </Switch>
     </div>
   );
