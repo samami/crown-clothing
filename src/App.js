3d9568e3ef303ff94df0fccd2bcff02ad60a5add
Sam Valenti <sam@arts-people.com>
2020-06-03 06:36:36 -1000

    Add sign up component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import Shoppage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import Entrypage from "./pages/entrypage/entrypage.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor(props) {
@@ -18,8 +18,20 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/pages/entrypage/entrypage.component.jsx
+++ src/pages/entrypage/entrypage.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./entrypage.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const Entrypage = () => {
   return (
     <div className="entrypage">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
