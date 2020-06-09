f840e7e23200ef74f62d74260ce6722f1f5e41f3
Sam Valenti <sam@arts-people.com>
2020-06-09 08:46:41 -1000

    Close cart dropdown when going to cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,12 +7,12 @@ import { createStructuredSelector } from "reselect";
 //Import components
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
-
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 //Import Styles
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = ({ cartItems, history }) => {
+const CartDropdown = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -24,7 +24,12 @@ const CartDropdown = ({ cartItems, history }) => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <CustomButton onClick={() => history.push("/checkout")}>
+      <CustomButton
+        onClick={() => {
+          history.push("/checkout");
+          dispatch(toggleCartHidden());
+        }}
+      >
         GO TO CHECKOUT
       </CustomButton>
     </div>
@@ -34,4 +39,5 @@ const CartDropdown = ({ cartItems, history }) => {
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
 });
+
 export default withRouter(connect(mapStateToProps)(CartDropdown));
